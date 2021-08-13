2a8121de5fe6c72a088d35f66f60e10a3cae26d3
tpeterke <tpeterke@gmail.com>
2021-08-12 18:46:59 -0700

    Create wa_accounts.py

---
+++ wa_accounts.py
@@ -0,0 +1,61 @@
+import requests
+import json
+import os
+import sys
+from datetime import datetime, timedelta, timezone
+import pytz
+
+
+
+accessToken = "<YOUR_FULL_ACCESS_TOKEN>"
+
+endpoint = "https://gql.waveapps.com/graphql/public"
+headers = {"Authorization": f"Bearer {accessToken}",
+           "Content-Type": "application/json"}
+
+query = """
+query ($businessId: ID!) {
+  business(id: $businessId) {
+    id
+    accounts(page: 1, pageSize: 100, subtypes: [UNCATEGORIZED_EXPENSE,UNCATEGORIZED_INCOME,CASH_AND_BANK]) {
+      pageInfo {
+        currentPage
+        totalPages
+        totalCount
+      }
+      edges {
+        node {
+          id
+          name
+          description
+          displayId
+          currency {
+              code
+          }
+          type {
+            name
+            value
+          }
+          subtype {
+            name
+            value
+          }
+          normalBalanceType
+          isArchived
+        }
+      }
+    }
+  }
+}
+         """
+
+variables = """
+{
+    "businessId": "<YOUR_BUSINESS_ID>"
+}
+"""
+
+
+r = requests.post(
+    endpoint, json={"query": query, "variables": variables}, headers=headers)
+print(json.dumps(r.json(), indent=2))
